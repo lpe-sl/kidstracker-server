5a0490683894ab0f234b1e04464bc258dfd2ba62
erplpe <erplpe123@gmail.com>
2023-03-21 10:00:29 +0530

    .

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../path/to/your/firebase-private-key.json");
+const serviceAccount = require("../key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
